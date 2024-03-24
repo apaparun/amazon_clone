34674cbcdd18551ce4e229cae1e47c5e83a844bb
apaparun <arunprasanth488@gmail.com>
2024-03-24 21:51:41 +0530

    add product api integrated

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -4,6 +4,7 @@ import 'package:amazon_clone/Constants/global_variables.dart';
 import 'package:amazon_clone/Constants/utils.dart';
 import 'package:amazon_clone/common/widget/custom_button.dart';
 import 'package:amazon_clone/common/widget/custom_textfield.dart';
+import 'package:amazon_clone/features/admin/services/admin_services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController descriptionCtrl = TextEditingController();
   final TextEditingController priceCtrl = TextEditingController();
   final TextEditingController quantityCtrl = TextEditingController();
+  final AdminServices adminServices = AdminServices();
+  final _addProductKey = GlobalKey<FormState>();
   String category = 'Mobiles';
   List<File> images = [];
   @override
@@ -40,6 +43,19 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+  void sellProduct() {
+    FocusScope.of(context).unfocus();
+    if (_addProductKey.currentState!.validate() && images.isNotEmpty) {
+      adminServices.sellProduct(
+          context: context,
+          name: productNameCtrl.text,
+          description: descriptionCtrl.text,
+          price: double.parse(priceCtrl.text),
+          quantity: double.parse(quantityCtrl.text),
+          category: category,
+          images: images);
+    }
+  }
 
   void selectImages() async {
     var res = await pickImages();
@@ -66,99 +82,102 @@ class _AddProductScreenState extends State<AddProductScreen> {
       ),
       body: SingleChildScrollView(
         child: Form(
+            key: _addProductKey,
             child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              images.isNotEmpty
-                  ? CarouselSlider(
-                      items: images.map((e) {
-                        return Builder(
-                            builder: (BuildContext context) => Image.file(
-                                  e,
-                                  fit: BoxFit.cover,
-                                  height: 200,
-                                ));
-                      }).toList(),
-                      options:
-                          CarouselOptions(viewportFraction: 1, height: 200),
-                    )
-                  : GestureDetector(
-                      onTap: selectImages,
-                      child: DottedBorder(
-                        borderType: BorderType.RRect,
-                        radius: const Radius.circular(10),
-                        dashPattern: const [10, 4],
-                        strokeCap: StrokeCap.round,
-                        child: Container(
-                          width: double.infinity,
-                          height: 150,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.folder_open,
-                                size: 40,
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  images.isNotEmpty
+                      ? CarouselSlider(
+                          items: images.map((e) {
+                            return Builder(
+                                builder: (BuildContext context) => Image.file(
+                                      e,
+                                      fit: BoxFit.cover,
+                                      height: 200,
+                                    ));
+                          }).toList(),
+                          options:
+                              CarouselOptions(viewportFraction: 1, height: 200),
+                        )
+                      : GestureDetector(
+                          onTap: selectImages,
+                          child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            radius: const Radius.circular(10),
+                            dashPattern: const [10, 4],
+                            strokeCap: StrokeCap.round,
+                            child: Container(
+                              width: double.infinity,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.folder_open,
+                                    size: 40,
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    "Select Product Images",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey.shade400),
+                                  )
+                                ],
                               ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                "Select Product Images",
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.grey.shade400),
-                              )
-                            ],
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-              const SizedBox(height: 30),
-              CustomFormField(
-                controller: productNameCtrl,
-                hinttext: "Product Name",
+                  const SizedBox(height: 30),
+                  CustomFormField(
+                    controller: productNameCtrl,
+                    hinttext: "Product Name",
+                  ),
+                  const SizedBox(height: 10),
+                  CustomFormField(
+                    controller: descriptionCtrl,
+                    hinttext: "Description",
+                    maxLines: 7,
+                  ),
+                  const SizedBox(height: 10),
+                  CustomFormField(
+                    controller: priceCtrl,
+                    hinttext: "Price",
+                  ),
+                  const SizedBox(height: 10),
+                  CustomFormField(
+                    controller: quantityCtrl,
+                    hinttext: "Quantity",
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: DropdownButton(
+                        value: category,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: productCategories.map((String item) {
+                          return DropdownMenuItem(
+                              value: item, child: Text(item));
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            category = newValue!;
+                          });
+                        }),
+                  ),
+                  const SizedBox(height: 10),
+                  CustomButton(text: "Sell", onTap: sellProduct)
+                ],
               ),
-              const SizedBox(height: 10),
-              CustomFormField(
-                controller: descriptionCtrl,
-                hinttext: "Description",
-                maxLines: 7,
-              ),
-              const SizedBox(height: 10),
-              CustomFormField(
-                controller: priceCtrl,
-                hinttext: "Price",
-              ),
-              const SizedBox(height: 10),
-              CustomFormField(
-                controller: quantityCtrl,
-                hinttext: "Quantity",
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                child: DropdownButton(
-                    value: category,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: productCategories.map((String item) {
-                      return DropdownMenuItem(value: item, child: Text(item));
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        category = newValue!;
-                      });
-                    }),
-              ),
-              const SizedBox(height: 10),
-              CustomButton(text: "Sell", onTap: () {})
-            ],
-          ),
-        )),
+            )),
       ),
     );
   }
--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
+import 'package:amazon_clone/Constants/error_handling.dart';
+import 'package:amazon_clone/Constants/global_variables.dart';
 import 'package:amazon_clone/Constants/utils.dart';
+import 'package:amazon_clone/models/product.dart';
+import 'package:amazon_clone/provider/user_provider.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class AdminServices {
   void sellProduct({
@@ -14,14 +20,37 @@ class AdminServices {
     required String category,
     required List<File> images,
   }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
     try {
       final cloudnary = CloudinaryPublic("dbz04pplg", "fk2b5xww");
       List<String> imageUrls = [];
       for (int i = 0; i < images.length; i++) {
-        CloudinaryResponse res =
-            await cloudnary.uploadFile(CloudinaryFile.fromFile(images[i].path));
-        imageUrls.add(res.secureUrl);
+        CloudinaryResponse cloudres = await cloudnary
+            .uploadFile(CloudinaryFile.fromFile(images[i].path, folder: name));
+        imageUrls.add(cloudres.secureUrl);
       }
+      Product product = Product(
+          name: name,
+          description: description,
+          quantity: quantity,
+          images: imageUrls,
+          category: category,
+          price: price);
+      print("${product.toJson()}");
+      http.Response res = await http.post(Uri.parse("$uri/admin/add-product"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': userProvider.user.token
+          },
+          body: product.toJson());
+
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, "Product added successfully");
+            Navigator.pop(context);
+          });
     } catch (e) {
       showSnackBar(context, e.toString());
     }
